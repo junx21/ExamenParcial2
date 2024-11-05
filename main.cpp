8b2dabfb11ebcf65a1e52490b66c8f822da25eab
junx21 <jxuche@myuax.com>
2024-11-05 11:06:37 +0100

    main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Ejercicio1/EJ1.h"
+#include "Ejercicio2/EJ2.h"
+#include "Ejercicio3/EJ3.h"
+#include "Ejercicio4/EJ4.h"
+
+
+int main() {
+
+    promocion();
+    identificacion();
+    semana();
+    promedio();
+
+    return 0;
+}
